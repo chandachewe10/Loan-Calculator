415b9595f8b574a85bcbd8f20d95954556313cb2
chanda chewe <chewec03@gmail.com>
2022-01-15 13:30:22 -0800

    Changed from javascript to VUEJS for dynamic calculations

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Loan Calculator
 
 
-This is a simple Loan Calculator that can be used to calculate one's total amount to be repaid within a given time-frame and how much he/she will be paying monthly in order to cover that loan amount. It can be integrated in loan management system websites.
+This is a simple Loan dynamic Calculator built with VUEJS that can be used to calculate one's total amount 
+to be repaid within a given time-frame (weekly loan repayments) and how much he/she
+ will be paying monthly in order to cover that loan amount.
+  It can be integrated in loan management system websites. 
--- assets/js/cal.js
+++ assets/js/cal.js
@@ -1,83 +1,67 @@
-var amount = document.getElementById("amount");
-var aoutput = document.getElementById("amountoutput");
-//Show output to the user
-aoutput.innerHTML='ZMW '+amount.value;
-//Update amount If scrolled
-amount.oninput = function() {
-  aoutput.innerHTML ='ZMW '+this.value;
-  aoutput.value=this.value;
-  
-  }
-
-var period = document.getElementById("period");
-var poutput = document.getElementById("periodoutput");
-//Show output to the user
-poutput.innerHTML=period.value +' Months';
-//Update period If scrolled
-period.oninput = function() {
-  poutput.innerHTML =this.value +' Months';
-  poutput.value=this.value;
-  
- }
-
- //Insert some starting figures on page load
- //On page load set the starting amount as 500 and period as 6 months
- window.onload=function(){
-//Amount 500 set on pageload
-  aoutput.value=500;
-  //Period 6 months set on page load  
-  poutput.value=6;
+new Vue({   
+  el: '#loan',  
+  data:{
+    amountoutput:"500",
+    periodoutput:"2",
+    amountToPay:"625",
+    InterestAmount:"125"
+  },
+  watch:{
+amountoutput:function(amount){
+this.amountoutput=amount;
+if(this.periodoutput==1){
+  this.amountToPay=Math.round(0.15*amount) + Math.round(amount);
+  this.InterestAmount=Math.round(0.15*amount);
 }
 
-
-//If calculate button is clicked, start the calculations  
-function loanAnalysis(){
-  
-
-//Start Calculations
-
-
-//Loan amount 
-var l = parseFloat(document.getElementById('amountoutput').value);
-
-//Processing fee of K130
-var proc = parseFloat(130);
-
-//Total Loan amount will be the loan amount plus the processing fee amount
-var p = parseFloat(l + proc);
-
-//Loan Interest rate per month, not per year but per month
-var r = 0.15; 
-
-//Number of periods of repaying the loan in months
-var n = parseFloat(document.getElementById('periodoutput').value);
-
-//Loan Monthly payments formular and results
-var f =  p *(r*Math.pow((1 + r),n)) / (Math.pow((1 + r),n-1));
-
-//To two decimal places monthly payments
-var a = f.toFixed(2); 
-
-
-//Total Payments
-var total = a * n;
-
-//To two decimal places final pay
-var finalPay = total.toFixed(2); 
-
-
-
-//Display this on Calculated results for the client to see
-
-//Monthly payments
-document.getElementById('monthly').innerHTML = a;
-
- // Total payments to be made
- document.getElementById('total').innerHTML = finalPay;
-
-
-
+if(this.periodoutput==2){
+  this.amountToPay=Math.round(0.25*amount) + Math.round(amount);
+  this.InterestAmount=Math.round(0.25*amount);
 }
 
 
 
+if(this.periodoutput==3){
+  this.amountToPay=Math.round(0.35*amount) + Math.round(amount);
+  this.InterestAmount=Math.round(0.35*amount);
+}
+
+
+
+if(this.periodoutput==4){
+  this.amountToPay=Math.round(0.45*amount) + Math.round(amount);
+  this.InterestAmount=Math.round(0.45*amount);
+}
+
+    },
+
+
+
+    periodoutput:function(period){
+      this.periodoutput=period;
+      if(period==1){
+        this.amountToPay=Math.round(0.15*this.amountoutput) + Math.round(this.amountoutput);
+        this.InterestAmount=Math.round(0.15*this.amountoutput);
+      }
+      
+      if(period==2){
+        this.amountToPay=Math.round(0.25*this.amountoutput) + Math.round(this.amountoutput);
+        this.InterestAmount=Math.round(0.25*this.amountoutput);
+      }
+      
+      
+      
+      if(period==3){
+        this.amountToPay=Math.round(0.35*this.amountoutput) + Math.round(this.amountoutput);
+        this.InterestAmount=Math.round(0.35*this.amountoutput);
+      }
+      
+      
+      
+      if(period==4){
+        this.amountToPay=Math.round(0.45*this.amountoutput) + Math.round(this.amountoutput);
+        this.InterestAmount=Math.round(0.45*this.amountoutput);
+      }
+}
+}
+  })
